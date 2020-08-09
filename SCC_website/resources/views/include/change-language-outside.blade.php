a7d92a7041f584486fecf4ebf9913781fd50c5b5
Huynh Ngoc Thien <gunshot1st@gmail.com>
2020-08-09 11:42:38 +0700

    Thien final commit before submitting for multi-disciplinary Project

--- SCC_website/resources/views/include/change-language-outside.blade.php
+++
@@ -1,9 +0,0 @@
-<select name="user_lang" id="select2bs4-language" style="padding-right: 30px" class="form-control select2bs4" style="width: 100%;">
-    @foreach( $lang_db as $lang_each)
-        @if (Cookie::get('1752051_user_lang') == $lang_each->language_code)
-            <option selected value="{{{$lang_each->language_code}}}">@lang($lang_each->language_name)</option>
-        @else
-            <option value="{{{$lang_each->language_code}}}">@lang($lang_each->language_name)</option>
-        @endif
-    @endforeach
-</select>
